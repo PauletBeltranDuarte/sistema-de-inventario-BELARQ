a359eecdf43d902626b82cda1e1f8cc8de8eb4a7
pau <pau@mail.com>
2024-05-16 19:27:18 -0700

    10/06/24

--- modal/registro_productos.php
+++ modal/registro_productos.php
@@ -11,7 +11,7 @@
 			<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Agregar nuevo producto</h4>
 		  </div>
 		  <div class="modal-body">
-			<form class="form-horizontal" method="post" id="guardar_producto" name="guardar_producto">
+			<form class="form-horizontal" method="post" id="guardar_producto" name="guardar_producto" enctype="multipart/form-data">
 			<div id="resultados_ajax_productos"></div>
 			  <div class="form-group">
 				<label for="codigo" class="col-sm-3 control-label">Código</label>
@@ -51,13 +51,21 @@
 				  <input type="text" class="form-control" id="precio" name="precio" placeholder="Precio de compra del producto" required pattern="^[0-9]{1,5}(\.[0-9]{0,2})?$" title="Ingresa sólo números con 0 ó 2 decimales" maxlength="8">
 				</div>
 			</div>
-			
+
+			<div class="form-group">
+			<label for="foto" class="col-sm-3 control-label">Foto</label>
+			<div class="col-sm-8">
+			<input type="file" class="form-control" name="foto" accept="image/jpeg" >
+			</div>
+			</div>
+
 			<div class="form-group">
 				<label for="stock" class="col-sm-3 control-label">Stock</label>
 				<div class="col-sm-8">
 				  <input type="number" min="0" class="form-control" id="stock" name="stock" placeholder="Inventario inicial" required  maxlength="8">
 				</div>
 			</div>
+
 			 
 			 
 			
